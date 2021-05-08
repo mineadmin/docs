ccfc568cd71a9f2eb59b8311bd25f722b6c63754
18638883567 <root@imoi.cn>
2021-05-08 15:49:10 +0800

    提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    title: 'MineAdmin 官网',
+    description: '基于Swoole驱动的Hyperf框架 & Vue 开发的常驻内存型后台管理框架。',
+    keywords: 'MineaAmin后台管理,Hyperf框架,Swoole架构,PHP,后台管理系统,前后端分离,vue,elementui'
+  }
