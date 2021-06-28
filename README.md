b70f34b5993002cb0806e7b8f655f07fdd8ed15b
18638883567 <root@imoi.cn>
2021-06-28 15:13:49 +0800

    提交

--- README.md
+++ README.md
@@ -3,15 +3,8 @@
 #### 介绍
 mineadmin开发文档
 
-#### 软件架构
-软件架构说明
-
-
 #### 安装教程
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
+`yarn install`
 
 #### 使用说明
 
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    title: 'MineAdmin 官网',
-    description: '基于Swoole驱动的Hyperf框架 & Vue 开发的常驻内存型后台管理框架。',
-    keywords: 'MineaAmin后台管理,Hyperf框架,Swoole架构,PHP,后台管理系统,前后端分离,vue,elementui'
-  }
+  base: '/',
+  title: 'test',
+  description: '基于Swoole驱动的Hyperf框架 & Vue 开发的常驻内存型后台管理框架。',
+  keywords: 'MineaAmin后台管理,Hyperf框架,Swoole架构,PHP,后台管理系统,前后端分离,vue,elementui',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
+  nav: [
+    { text: 'Home', link: '/' },
+    { text: 'About', link: '/about/' },
+  ]
+}
