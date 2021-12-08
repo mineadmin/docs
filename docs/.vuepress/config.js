d2d07cd23e76917825571056aff16ecf13a52d7b
莫羿 <261091613@qq.com>
2021-12-08 17:04:36 +0800

    up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,42 @@
 module.exports = {
   title: 'MineAdmin',
+  base: '/',
   description: '快速构建你的web应用程序',
-  // theme: 'reco',
+
   markdown: {
     lineNumbers: true,
   },
   themeConfig: {
     logo: '/logo.svg',
     
-
     head: [
       ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
     ],
 
     nav: [
       { text: '文档', link: '/doc/' },
+      { text: '捐赠列表', link: '/donation'},
       { text: '演示', link: 'https://demo.mineadmin.com' },
       { text: 'Gitee', link: 'https://gitee.com/xmo/MineAdmin' },
     ],
 
+    sidebarDepth: 1,
     sidebar: {
+      "/guide/": [
+        {
+          title: '开始',
+          collapsable: false,
+					children: [
+						['/guide/', '介绍'],
+						['/guide/', '路由'],
+						['/guide/style', '布局样式'],
+						['/guide/server', '和服务端进行交互'],
+						['/guide/newPage', '第一个页面'],
+						['/guide/build', '构建与部署']
+					]
+        }
+      ],
+
       "/doc/": [
         '/doc/',
         '/doc/install',
