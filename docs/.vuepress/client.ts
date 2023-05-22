340af9de573fa33d34b9a375ef9f4ef481ca1daa
X.Mo <root@imoi.cn>
2023-05-22 14:17:10 +0800

    up

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,10 +1,9 @@
 import { defineClientConfig } from '@vuepress/client'
 import ArcoVue from '@arco-design/web-vue'
-import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 import '@arco-design/web-vue/dist/arco.css'
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
-    app.use(ArcoVue, {}).use(ArcoVueIcon)
+    app.use(ArcoVue, {})
   },
 })
